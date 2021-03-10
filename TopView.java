b5dd597c7be4ea759dfeb2e01c3fffad492fadeb
Rajat <59741387+evilninja6726@users.noreply.github.com>
2021-03-11 00:09:05 +0530

    Add files via upload
    Printing the Top View of a Binary Tree in O(n*hd) Time and O(hd) Space where hd is the maximum horizontal distance from left to right in a Binary Tree.

---
+++ TopView.java
@@ -0,0 +1,39 @@
+package binarySearchTreeDs;
+
+import java.util.ArrayDeque;
+import java.util.Map;
+import java.util.Queue;
+import java.util.TreeMap;
+
+public class TopView {
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.left.left = new Node(40);
+        root.left.right = new Node(50);
+        topView(root);
+    }
+
+    public static void topView(Node root) {
+        if (root == null)
+            return;
+        Queue<Pair> queue = new ArrayDeque<>();
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        queue.offer(new Pair(root, 0));
+        while (!queue.isEmpty()) {
+            Pair p = queue.poll();
+            Node curr = p.node;
+            if (!treeMap.containsKey(p.hDistance)) {
+                treeMap.put(p.hDistance, curr.data);
+            }
+            if (curr.left != null)
+                queue.offer(new Pair(curr.left, p.hDistance - 1));
+            if (curr.right != null)
+                queue.offer(new Pair(curr.right, p.hDistance + 1));
+        }
+        for (Map.Entry<Integer, Integer> entry : treeMap.entrySet()) {
+            System.out.print(entry.getValue() + " ");
+        }
+    }
+}
